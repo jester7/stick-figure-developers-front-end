bce96395b9b3e56e224397628a1ee9106f1c032d
Jovan Jester <jovan@jester.cafe>
2021-11-26 00:46:41 -0600

    changed title to Stick Figure Developers NFTs

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">Stick Figure Developers NFTs</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Each unique. Each beautiful. Discover your Stick Figure Developer NFT today.
           </p>
           {renderNotConnectedContainer()}
         </div>
