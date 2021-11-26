d92f5258e3e56961b9944f763eeed0f010a67432
Jovan Jester <jovan@jester.cafe>
2021-11-26 01:23:13 -0600

    forgot ethers import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import stickFigureDevelopersNFT from './utils/StickFigureDevelopersNFT.json';
 
 // Constants
