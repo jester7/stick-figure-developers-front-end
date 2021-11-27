e31e33f90ef911dc248f421bb4c02db9725c5afb
Jovan Jester <jovan@jester.cafe>
2021-11-27 16:59:40 -0600

    fixed missing call to setupEventListener() if wallet already authorized

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ const App = () => {
     if (accounts.length !== 0) {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
-      setCurrentAccount(account)
+      setCurrentAccount(account);
+      setupEventListener();
     } else {
       console.log("No authorized account found")
     }
