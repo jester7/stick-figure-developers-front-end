9f9bea57621a15c1c98bf157bd0a997bdad7d29d
Jovan Jester <jovan@jester.cafe>
2021-11-28 03:14:51 -0600

    fixed bugs and styles improved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import sampleDeveloper from './assets/sample-developer.svg';
 import mintingAnimation from './assets/pick-axe.svg';
 
 import React, { useEffect, useState } from "react";
@@ -14,7 +15,7 @@ const TOTAL_MINT_COUNT = 50;
 
 //const CONTRACT_ADDRESS = "0xE6DDc9b40FbA5f8643F6b4080EBE2C08516e915C";
 //const CONTRACT_ADDRESS = "0xe7c1dCb0bA5C2e3e1061150C387Ff2534258639C"; // removed hardhat console, testing errors and events
-const CONTRACT_ADDRESS = "0xCB0Cae20BB14412dB346Dd96Ce75592a2911c5D5";
+const CONTRACT_ADDRESS = "0xD27a248980C427B80Ae8a794684C4280B3af0269";
 
 const App = () => {
 
@@ -22,9 +23,15 @@ const App = () => {
     * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
     */
   const [currentAccount, setCurrentAccount] = useState("");
+  const [message, setMessage] = useState("");
+
   const [isMinting, setIsMinting] = useState(false); // state variable for minting/mining animation
   const [lastMintedNFT, setLastMintedNFT] = useState("");
+  const [mintedCount, setMintedCount] = useState(0);
+  const [maxSupply, setMaxSupply] = useState(0);
   
+
+
   const checkIfWalletIsConnected = async () => {
     /*
     * First make sure we have access to window.ethereum
@@ -108,9 +115,11 @@ const App = () => {
         // This will essentially "capture" our event when our contract throws it.
         // If you're familiar with webhooks, it's very similar to that!
         connectedContract.on("NewDeveloper", (from, tokenId) => {
-          setLastMintedNFT(`"https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}"`);
+          setLastMintedNFT(`https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
           console.log(from, tokenId.toNumber());
-          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          getMintedCount();
+          setMessage("We've minted your NFT and sent it to your wallet. It may be blank right now. It can take up to 10 minutes to show up on OpenSea.");
+          
         });
 
         console.log("Setup event listener!")
@@ -139,9 +148,11 @@ const App = () => {
         let nftTxn = await connectedContract.createDeveloper();
 
         console.log("Mining... please wait.");
+        setMessage("Minting in progress.");
         setIsMinting(true);
         await nftTxn.wait();
         setIsMinting(false);
+        setMessage("");
 
         console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
 
@@ -150,6 +161,8 @@ const App = () => {
       }
     } catch (error) {
       setIsMinting(false);
+      setMessage("Something went wrong 😢, please try minting again.");
+
       setLastMintedNFT("");
       console.log(error)
     }
@@ -176,7 +189,6 @@ const App = () => {
     }
   };
 
-  /*
   const renderLastNftLink = () => {
     if (lastMintedNFT !== "") {
       return (
@@ -184,11 +196,11 @@ const App = () => {
       );
     }
 
-    return (<span></span>);
-  }; */
+    return "";
+  };
   
   const getMintedCount = async () => {
-    try {
+      try {
       const { ethereum } = window;
 
       if (ethereum) {
@@ -199,19 +211,19 @@ const App = () => {
         let devCount = await connectedContract.getDeveloperCount();
 
         console.log("Checking nft count...");
-        //await devCount.wait();
 
         console.log(`got this as devCount: ${devCount}`);
-        return devCount;
+        setMintedCount(devCount.toNumber());
       } else {
         console.log("ethereum object doesn't exist");
       }
     } catch (error) {
       console.log(error)
-    }
+    }  
   };
 
-  const getMaxSupply = async () => {
+  const getMaxNFTs = async () => {
+    //if (maxSupply != 0) return;
     try {
       const { ethereum } = window;
 
@@ -225,33 +237,48 @@ const App = () => {
         console.log("Checking max supply...");
         //await max.wait();
 
-        console.log(`got this for max supply: ${max}`);
-        return max;
+        console.log("got max supply> %d", max.toNumber());
+        setMaxSupply(max.toNumber());
       } else {
         console.log("ethereum object doesn't exist");
       }
     } catch (error) {
-      console.log(error)
+      console.log("got error inside getMaxSupply");
+      //console.log(error)
     }
+  
   };
 
   useEffect(() => {
     checkIfWalletIsConnected();
-  }, [])
+    getMaxNFTs();
+    getMintedCount();
+  }, []);
 
+//   useEffect(() => {
+// //    getMintedCount();
+
+//   }, []);
+
+  
+  
+  
 
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">Stick Figure Developers NFTs</p>
+          <p className="header gradient-text">Stick Figure Developer NFTs</p>
           <p className="sub-text">
-            Your Stick Figure Developer NFT will be minted with a random facial expression, colorful clothing, and programming language. On rare occasions, you will get a mythical <em>10x</em> developer.
+            Your Stick Figure Developer NFT will be minted
+            with a random facial expression, colorful clothing,
+            and a programming language. On rare occasions, you will get a mythical <em>10x developer</em>.
           </p>
-          <p class="counter">
-            A total of <span>{getMintedCount()}</span> out of <span>{getMaxSupply()}</span> have been minted.
+          <p className="counter">
+            A total of <span>{mintedCount}</span> out of <span>{maxSupply}</span> have been minted.
           </p>
-          {/* <p class="last-nft">{renderLastNftLink()}</p> */}
+          <p className="last-nft">{renderLastNftLink()}</p>
+          <p className="message">{message}</p>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
@@ -260,6 +287,7 @@ const App = () => {
             </button>
           )}
           {renderMintingAnimation()}
+          <img className="sample" alt="sample Stick Figure Developer NFT" src={sampleDeveloper} />
         </div>
         
         <div className="footer-container">
@@ -269,7 +297,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
